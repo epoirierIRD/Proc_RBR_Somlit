aadd91a397bba00c6c5e06a486579d7c0401f48a
epoirierIRD <etienne.poirier@ird.fr>
2025-06-17 08:49:00 +0200

    updated

--- 20240606/main.py
+++ 20240606/main.py
@@ -13,6 +13,7 @@ import os
 import math
 import RSKsomlit_plt as rsksplt
 import RSKsomlit_proc as rsksproc
+import sensor_uncertainties as sun
 
 
 # ------------------------------------MAIN-------------------------------------
@@ -20,7 +21,8 @@ import RSKsomlit_proc as rsksproc
 # Main
 # pc path
 path_in = "/home/epoirier/Documents/PROJETS/2025/Proc_RBR_Somlit/20240606/231853_20240607_1104 somlit 21.rsk"
-path_out = "/home/epoirier/Documents/PROJETS/2025/Proc_RBR_Somlit/20240606"
+# Get the directory of main.py
+path_out = os.path.dirname(os.path.abspath(__file__))
 
 
 # enter parameters of your local processing and somlit point
@@ -34,16 +36,47 @@ latitude = 48.35
 raw,rsk,rsk_d,rsk_u, profile_nb = rsksproc.procRSK (path_in, patm, latitude, path_out)
 
 #plotting
-for param in ['salinity', 'temperature', 'chlorophyll-a']:
-    
-    
-    rsksplt.plot_up_down(rsk_d, rsk_u, param, profile_nb)
-    
+for param in [
+      'conductivity',
+      'temperature',
+      #'pressure',
+      'temperature1',
+      'dissolved_o2_concentration',
+      'par',
+      'ph',
+      'chlorophyll-a',
+      'fdom',
+      'turbidity',
+      # 'sea_pressure',
+      # 'depth',
+      'salinity',
+      # 'speed_of_sound',
+      # 'specific_conductivity',
+      # 'dissolved_o2_saturation',
+      # 'velocity',
+      # 'density_anomaly'
+    ]:
        
-    for cast in ['down','up']:
-        if cast == 'down':
-            fig1, axes1 = rsksplt.plot_raw_proc(rsk_d, rsk, param, cast, profile_nb)
-        else:
-            fig2, axes2 = rsksplt.plot_raw_proc(rsk_u, rsk, param, cast, profile_nb)
+    
+    # plot up and down casts processed on the same graph with uncertainties
+    # we can see that the been avergaging is not working on upcast: points are not aligned
+    # to be solved 
+    rsksplt.plot_up_down2(rsk_d, rsk_u, param, profile_nb, path_out)
 
+    # plot raw and processed data for up and down casts on differents profiles    
+
+    # get the uncertainty for each param to use in the plots below
+    # uncertainty = sun.get_uncertainty(param)
+       
+    # for cast in ['down','up']:
+    #     if cast == 'down':
+    #         fig1, axes1 = rsksplt.plot_raw_proc(rsk_d, rsk, param, cast, profile_nb, uncertainty)
+    #     else:
+    #         fig2, axes2 = rsksplt.plot_raw_proc(rsk_u, rsk, param, cast, profile_nb, uncertainty)
+
+# Add here a function to trim de chosen cast up or down once the graphs have been checked
+
+# function to convert the downcast file into the good format for SOMLIT DB
+rsksproc.toSomlitDB('/home/epoirier/Documents/PROJETS/2025/Proc_RBR_Somlit/20240606/downcast/231853_20240607_1104 somlit 21_profile0.csv',
+                    '/home/epoirier/Documents/PROJETS/2025/Proc_RBR_Somlit/20240606/downcast/4Somlit.csv')
 
